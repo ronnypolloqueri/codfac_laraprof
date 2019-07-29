3dc953d19af94d7385a6f4f9e66d1d3574874ae5
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2019-07-29 04:56:42 -0500

    NavBar, add links to posts and my posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.index') }}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.my') }}">My Posts</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
